43ec4d248cb49c94522c7e13983e3fe4accedbad
Ahmad Cooper <108217835+AhmadAC@users.noreply.github.com>
2023-01-20 12:51:56 +0700

    autoupdate

---
+++ Batch Scripts/Push Github Repo.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "autoupdate"
+git branch -M main
+@REM change to your username: https://github.com/USERNAME/
+git remote add origin https://github.com/AhmadAC/
+git push -u origin main
