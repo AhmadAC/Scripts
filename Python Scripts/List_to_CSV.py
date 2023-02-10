c0f2fbeb43694532afc2699a32610d372f890c5d
Ahmad Cooper <108217835+AhmadAC@users.noreply.github.com>
2023-02-10 12:32:26 +0700

    autoupdate

--- Python Scripts/List_to_CSV.py
+++ Python Scripts/List_to_CSV.py
@@ -1,17 +1,12 @@
 # Written by Ahmad Cooper
 # pip install pyperclip ; pyperclip doesn't work on Android
 import pyperclip
-import sys
 
-sys.argv
 # Retrieve the contents of the clipboard and strip new lines
 clipboard = pyperclip.paste().strip("\n")
 
 # List comprehension to join the list of words into a single string separated by newline characters
-# type any argument after calling the file and it'll make List to CSV
-if len(sys.argv) >1:
-    final = ', '.join([word.strip() for word in clipboard.split("\n")])
-else:
-    final = '\n'.join([word.strip() for word in clipboard.split(",")])
+final = ', '.join([word.strip() for word in clipboard.split("\n")])
+
 # Copy the final string to the clipboard
 pyperclip.copy(final)
